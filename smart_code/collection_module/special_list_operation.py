183c5e43180c367ee2049c1413dcd5d81d324049
RICHIE-RT <102750081+RICHIE-RT@users.noreply.github.com>
2022-09-23 21:28:57 +0530

    collection.deque added

---
+++ smart_code/collection_module/special_list_operation.py
@@ -0,0 +1,34 @@
+'''
+Input Format :
+The first line contains an integer N, the number of operations.
+The next N lines contains the space separated names of methods and their values.
+
+Output Format :
+Print the space separated elements of deque d.
+
+Sample Input :              Sample Output:
+6                           1 2
+append 1
+append 2
+append 3
+appendleft 4
+pop
+popleft
+'''
+
+from collections import deque
+
+d = deque()
+for i in range(int(input())):
+    s = input().split()
+    if s[0] == 'append':
+        d.append(s[1])
+    if s[0] == 'appendleft':
+        d.appendleft(s[1])
+    if s[0] == 'pop':
+        d.pop()
+    if s[0] == 'popleft':
+        d.popleft()
+    
+for j in d:
+    print(j, end = ' ')
